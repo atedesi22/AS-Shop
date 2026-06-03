267ac97f0083a86cf047937814a427eab9e8e4e0
atedesi22 <atedesi22@github.com>
2026-06-03 18:57:09 +0100

    ajout des produits

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,26 +1,6 @@
 export const products = [
   {
     id: 1,
-    name: "Jordan 4 Black Cat",
-    price: "165 000 FCFA",
-    category: "Outfits",
-    brand: "Nike",
-    description: "La célèbre Jordan 4 dans son coloris Black Cat, un classique indémodable.",
-    image: "/asshop3.JPG", // Image principale
-    gallery: ["/asshop3.JPG", "/asshop7.JPG", "/asshop11.JPG", "/asshop14.JPG"] // Galerie pour le détail
-  },
-  {
-    id: 2,
-    name: "Nike Dunk Low Panda",
-    price: "85 000 FCFA",
-    category: "Pulls",
-    brand: "Nike",
-    description: "Le design emblématique bicolore, polyvalent pour toutes les tenues.",
-    image: "/asshop4.JPG",
-    gallery: ["/asshop4.JPG", "/asshop8.JPG", "/asshop12.JPG", "/asshop10.JPG"]
-  },
-  {
-    id: 3,
     name: "Nike Air Force 1 Blue",
     price: "15 000 FCFA",
     category: "Sneakers",
@@ -30,7 +10,7 @@ export const products = [
     gallery: ["/sneakersNike/sneakerbleu3.jpg", "/sneakersNike/sneakerbleu2.jpg", "/sneakersNike/sneakerbleu1.jpg"]
   },
   {
-    id: 4,
+    id: 2,
     name: "Nike Air Force 1 Brown",
     price: "15 000 FCFA",
     category: "Sneakers",
@@ -40,37 +20,7 @@ export const products = [
     gallery: ["/sneakersNike/sneakermarron1.jpg", "/sneakersNike/sneakermarron2.jpg", "/sneakersNike/sneakermarron3.jpg"]
   },
   {
-    id: 5,
-    name: "Sacoche Techwear",
-    price: "35 000 FCFA",
-    category: "Sneakers",
-    brand: "Puma",
-    description: "Accessoire fonctionnel et durable, conçu pour le transport quotidien.",
-    image: "/asshop6.JPG",
-    gallery: ["/asshop6.JPG", "/asshop10.JPG", "/asshop13.JPG", "/asshop7.JPG"]
-  },
-  {
-    id: 6,
-    name: "AS Shop T-shirt",
-    price: "25 000 FCFA",
-    category: "T-shirts",
-    brand: "Jordan",
-    description: "T-shirt basique avec le logo AS Shop, un must-have pour tous les fans.",
-    image: "/asshop1.JPG",
-    gallery: ["/asshop1.JPG", "/asshop14.JPG", "/asshop5.JPG", "/asshop6.JPG"]
-  },
-  {
-    id: 7,
-    name: "AS Shop Cap",
-    price: "15 000 FCFA",
-    category: "Accessoires",
-    brand: "Adidas",
-    description: "Casquette stylée avec le logo AS Shop, parfaite pour compléter votre look.",
-    image: "/asshop2.JPG",
-    gallery: ["/asshop2.JPG", "/asshop13.JPG", "/asshop12.JPG", "/asshop9.JPG"]
-  },
-  {
-    id: 8,
+    id: 3,
     name: "Jean Fit bleu",
     price: "15 000 FCFA",
     category: "Pantalons",
@@ -80,7 +30,7 @@ export const products = [
     gallery: ["/pantalon/jeanbleu2.jpg", "/pantalon/jeanbleu5.jpg", "/pantalon/jeanbleu3.jpg", "/pantalon/jeanbleu4.jpg"]
   },
   {
-    id: 9 ,
+    id: 4,
     name: "Jean Fit noir",
     price: "15 000 FCFA",
     category: "Pantalons",
@@ -90,7 +40,7 @@ export const products = [
     gallery: ["/pantalon/jeannoir.jpg", "/pantalon/jeannoir2.jpg"]
   },
   {
-    id: 10,
+    id: 5,
     name: "Pantalon Mr. Tee",
     price: "15 000 FCFA",
     category: "Pantalons",
@@ -100,7 +50,7 @@ export const products = [
     gallery: ["/pantalon/pantalonmr.jpg", "/pantalon/pantalonmr3.jpg", "/pantalon/pantalonmr7.jpg", "/pantalon/pantalonmr4.jpg"]
   },
   {
-    id: 11,
+    id: 6,
     name: "Pantalon Mr. Tee",
     price: "15 000 FCFA",
     category: "Pantalons",
@@ -110,7 +60,7 @@ export const products = [
     gallery: ["/pantalon/pantalonmr5.jpg", "/pantalon/pantalonmr6.jpg", "/pantalon/pantalonmr8.jpg", "/pantalon/pantalonmr9.jpg", "/pantalon/pantalonmr10.jpg"]
   },
   {
-    id: 12,
+    id: 7,
     name: "Jean DSQUARED2",
     price: "15 000 FCFA",
     category: "Pantalons",
