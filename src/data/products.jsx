4d2021e81584c2ae2e863813cd0fbd6d5ef11e3e
atedesi22 <atedesi22@github.com>
2026-09-14 20:12:08 +0100

    MAJ

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,81 +1,81 @@
 export const products = [
-  {
-    id: 1,
-    name: "Nike Air Force 1 Blue",
-    price: "15 000 FCFA",
-    category: "Sneakers",
-    brand: "Nike",
-    sizes: [40, 41, 42, 43, 44, 45],
-    description: "Confort premium avec une coupe oversized, parfait pour le style streetwear.",
-    image: "/sneakersNike/sneakerbleu3.jpg",
-    gallery: ["/sneakersNike/sneakerbleu3.jpg", "/sneakersNike/sneakerbleu2.jpg", "/sneakersNike/sneakerbleu1.jpg"]
-  },
-  {
-    id: 2,
-    name: "Nike Air Force 1 Brown",
-    price: "15 000 FCFA",
-    category: "Sneakers",
-    brand: "Nike",
-    sizes: [40, 41, 42, 43, 44],
-    description: "Confort premium avec une coupe oversized, parfait pour le style streetwear.",
-    image: "/sneakersNike/sneakermarron1.jpg",
-    gallery: ["/sneakersNike/sneakermarron1.jpg", "/sneakersNike/sneakermarron2.jpg", "/sneakersNike/sneakermarron3.jpg"]
-  },
-  {
-    id: 3,
-    name: "Jean Fit bleu",
-    price: "15 000 FCFA",
-    category: "Pantalons",
-    brand: "Puma",
-    sizes: [], // Pas de pointure pour les pantalons
-    description: "Jean slim fit pour un look moderne et épuré, idéal pour toutes les occasions.",
-    image: "/pantalon/jeanbleu.jpg",
-    gallery: ["/pantalon/jeanbleu2.jpg", "/pantalon/jeanbleu5.jpg", "/pantalon/jeanbleu3.jpg", "/pantalon/jeanbleu4.jpg"]
-  },
-  {
-    id: 4,
-    name: "Jean Fit noir",
-    price: "15 000 FCFA",
-    category: "Pantalons",
-    brand: "Puma",
-    sizes: [],
-    description: "Jean slim fit noir pour un look moderne et épuré, idéal pour toutes les occasions.",
-    image: "/pantalon/jeannoir.jpg",
-    gallery: ["/pantalon/jeannoir.jpg", "/pantalon/jeannoir2.jpg"]
-  },
-  {
-    id: 5,
-    name: "Pantalon Mr. Tee",
-    price: "15 000 FCFA",
-    category: "Pantalons",
-    brand: "Puma",
-    sizes: [],
-    description: "Pantalon stylé avec la reference Mr. Tee, parfait pour compléter votre look.",
-    image: "/pantalon/pantalonmr.jpg",
-    gallery: ["/pantalon/pantalonmr.jpg", "/pantalon/pantalonmr3.jpg", "/pantalon/pantalonmr7.jpg", "/pantalon/pantalonmr4.jpg"]
-  },
-  {
-    id: 6,
-    name: "Pantalon Mr. Tee",
-    price: "15 000 FCFA",
-    category: "Pantalons",
-    brand: "Puma",
-    sizes: [],
-    description: "Pantalon stylé avec la reference Mr. Tee, parfait pour compléter votre look.",
-    image: "/pantalon/pantalonmr5.jpg",
-    gallery: ["/pantalon/pantalonmr5.jpg", "/pantalon/pantalonmr6.jpg", "/pantalon/pantalonmr8.jpg", "/pantalon/pantalonmr9.jpg", "/pantalon/pantalonmr10.jpg"]
-  },
-  {
-    id: 7,
-    name: "Jean DSQUARED2",
-    price: "15 000 FCFA",
-    category: "Pantalons",
-    brand: "Puma",
-    sizes: [],
-    description: "Pantalon DSQUARED pour un look utilitaire et tendance, idéal pour les sorties décontractées.",
-    image: "/pantalon/jeandsquared.jpg",
-    gallery: ["/pantalon/jeandsquared.jpg", "/pantalon/jeandsquared2.jpg"]
-  },
+  // {
+  //   id: 1,
+  //   name: "Nike Air Force 1 Blue",
+  //   price: "15 000 FCFA",
+  //   category: "Sneakers",
+  //   brand: "Nike",
+  //   sizes: [40, 41, 42, 43, 44, 45],
+  //   description: "Confort premium avec une coupe oversized, parfait pour le style streetwear.",
+  //   image: "/sneakersNike/sneakerbleu3.jpg",
+  //   gallery: ["/sneakersNike/sneakerbleu3.jpg", "/sneakersNike/sneakerbleu2.jpg", "/sneakersNike/sneakerbleu1.jpg"]
+  // },
+  // {
+  //   id: 2,
+  //   name: "Nike Air Force 1 Brown",
+  //   price: "15 000 FCFA",
+  //   category: "Sneakers",
+  //   brand: "Nike",
+  //   sizes: [40, 41, 42, 43, 44],
+  //   description: "Confort premium avec une coupe oversized, parfait pour le style streetwear.",
+  //   image: "/sneakersNike/sneakermarron1.jpg",
+  //   gallery: ["/sneakersNike/sneakermarron1.jpg", "/sneakersNike/sneakermarron2.jpg", "/sneakersNike/sneakermarron3.jpg"]
+  // },
+  // {
+  //   id: 3,
+  //   name: "Jean Fit bleu",
+  //   price: "15 000 FCFA",
+  //   category: "Pantalons",
+  //   brand: "Puma",
+  //   sizes: [], // Pas de pointure pour les pantalons
+  //   description: "Jean slim fit pour un look moderne et épuré, idéal pour toutes les occasions.",
+  //   image: "/pantalon/jeanbleu.jpg",
+  //   gallery: ["/pantalon/jeanbleu2.jpg", "/pantalon/jeanbleu5.jpg", "/pantalon/jeanbleu3.jpg", "/pantalon/jeanbleu4.jpg"]
+  // },
+  // {
+  //   id: 4,
+  //   name: "Jean Fit noir",
+  //   price: "15 000 FCFA",
+  //   category: "Pantalons",
+  //   brand: "Puma",
+  //   sizes: [],
+  //   description: "Jean slim fit noir pour un look moderne et épuré, idéal pour toutes les occasions.",
+  //   image: "/pantalon/jeannoir.jpg",
+  //   gallery: ["/pantalon/jeannoir.jpg", "/pantalon/jeannoir2.jpg"]
+  // },
+  // {
+  //   id: 5,
+  //   name: "Pantalon Mr. Tee",
+  //   price: "15 000 FCFA",
+  //   category: "Pantalons",
+  //   brand: "Puma",
+  //   sizes: [],
+  //   description: "Pantalon stylé avec la reference Mr. Tee, parfait pour compléter votre look.",
+  //   image: "/pantalon/pantalonmr.jpg",
+  //   gallery: ["/pantalon/pantalonmr.jpg", "/pantalon/pantalonmr3.jpg", "/pantalon/pantalonmr7.jpg", "/pantalon/pantalonmr4.jpg"]
+  // },
+  // {
+  //   id: 6,
+  //   name: "Pantalon Mr. Tee",
+  //   price: "15 000 FCFA",
+  //   category: "Pantalons",
+  //   brand: "Puma",
+  //   sizes: [],
+  //   description: "Pantalon stylé avec la reference Mr. Tee, parfait pour compléter votre look.",
+  //   image: "/pantalon/pantalonmr5.jpg",
+  //   gallery: ["/pantalon/pantalonmr5.jpg", "/pantalon/pantalonmr6.jpg", "/pantalon/pantalonmr8.jpg", "/pantalon/pantalonmr9.jpg", "/pantalon/pantalonmr10.jpg"]
+  // },
+  // {
+  //   id: 7,
+  //   name: "Jean DSQUARED2",
+  //   price: "15 000 FCFA",
+  //   category: "Pantalons",
+  //   brand: "Puma",
+  //   sizes: [],
+  //   description: "Pantalon DSQUARED pour un look utilitaire et tendance, idéal pour les sorties décontractées.",
+  //   image: "/pantalon/jeandsquared.jpg",
+  //   gallery: ["/pantalon/jeandsquared.jpg", "/pantalon/jeandsquared2.jpg"]
+  // },
 
   {
     id: 8,
@@ -160,14 +160,14 @@ export const products = [
 
   {
     id: 15,
-    name: "Jean DSQUARED2",
+    name: "Sneaker Nike",
     price: "15 000 FCFA",
-    category: "Pantalons",
-    brand: "Puma",
-    sizes: [],
-    description: "Pantalon DSQUARED pour un look utilitaire et tendance, idéal pour les sorties décontractées.",
-    image: "/pantalon/jeandsquared.jpeg",
-    gallery: ["/pantalon/jeandsquared.jpeg", "/pantalon/jeandsquared2.jpeg"]
+    category: "Sneakers",
+    brand: "Nike",
+    sizes: [37, 38, 39, 40, 41, 42, 43, 44, 45],
+    description: "Sneaker Nike pour un look moderne et épuré, idéal pour toutes les occasions.",
+    image: "/sneakersNike/nike1.jpeg",
+    gallery: ["/sneakersNike/nike1.jpeg", "/sneakersNike/nike2.jpeg"]
   },
 
   {
